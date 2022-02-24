66129e6c6498e09ae6fd5cec0e816a95db0ae56c
kein7z <rom.pokidov@gmail.com>
2022-02-24 14:09:35 +0700

    Добавляет console.error(); к выводу ошибки при вводе данных

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 //Функция, возвращающая случайное число
-function getRandomInRange (minNumber, maxNumber) {
-    if (minNumber >= 0 && maxNumber > minNumber) {
-        return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
-    }
-    return 'invalid input value'
+function getRandomInRange(minNumber, maxNumber) {
+  if (minNumber >= 0 && maxNumber > minNumber) {
+    return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+  }
+  return console.error('invalid input value');
 }
 
 getRandomInRange(1, 10);
 
 //Функция, возвращающая случайное число с плавающей точкой
-function getRandomСoord (minNumber, maxNumber, numOfDecimalPlaces) {
-    if (minNumber >= 0 && maxNumber > minNumber) {
-        return (Math.random() * (maxNumber - minNumber) + minNumber).toFixed(numOfDecimalPlaces);
-    }
-    return 'invalid input value'
+function getRandomСoord(minNumber, maxNumber, numOfDecimalPlaces) {
+  if (minNumber >= 0 && maxNumber > minNumber) {
+    return (Math.random() * (maxNumber - minNumber) + minNumber).toFixed(numOfDecimalPlaces);
+  }
+  return console.error('invalid input value');
 }
 
 getRandomСoord(1.3, 1.5, 4);
