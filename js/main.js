5254e2a742bc94e0a9b6218688fc758687aaf594
kein7z <rom.pokidov@gmail.com>
2022-03-22 14:24:39 +0700

    Добавление функций перевода страницы в активное/неактивное состояние

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ import { createOffers } from './create-offers.js';
 import './popup.js';
 import './validation/validation-rooms-guests.js';
 import './validation/validation-price-title.js';
-
+import './page-activation/inactivated-page.js'
+import { inactivatedPage } from './page-activation/inactivated-page.js';
 
 createOffers();
+inactivatedPage();
---
+++ js/page-activation/inactivated-page.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('.ad-form');
+const formElements = Array.from(form.children);
+const mapFilters = document.querySelector('.map__filters');
+const mapElements = Array.from(mapFilters.children);
+
+const inactivatedPage = () => {
+  form.classList.add('ad-form--disabled');
+  formElements.forEach((element) => element.setAttribute('disabled', ''));
+  mapFilters.classList.add('map__filters--disabled');
+  mapElements.forEach((element) => element.setAttribute('disabled', ''));
+};
+
+export {form, formElements, mapFilters, mapElements, inactivatedPage};
