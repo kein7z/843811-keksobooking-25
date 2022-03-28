af83791543ac8c91bd2a6b8c624ea1a1d5a428ec
Roman Pokkidov <rom.pokidov@gmail.com>
2022-03-28 14:31:49 +0700

    Добавляет интерактивну карту и слайдер для ввода цены

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ import './popup.js';
 import './validation/validation-rooms-guests.js';
 import './validation/validation-price-title.js';
 import './page-activation/inactivated-page.js';
-import { inactivatedPage } from './page-activation/inactivated-page.js';
+import './map/map.js';
+import './slider-price.js';
 
 createOffers();
-inactivatedPage();
