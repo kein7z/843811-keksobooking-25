498052a5ed7cf579cfe29b560769c1da14a85339
Roman Pokkidov <rom.pokidov@gmail.com>
2022-04-14 12:39:23 +0700

    добавил отображение ошибки загрузки данных и блокировки формы фильтрации

--- js/util.js
+++ js/util.js
@@ -6,4 +6,36 @@ const debounce = (callback, timeoutDelay) => {
   };
 };
 
-export { debounce };
+const formFilter = document.querySelector('.map__filters');
+const formFilterElements = Array.from(formFilter.children);
+const disableFormFilters = () => {
+  formFilterElements.forEach((element) => {
+    element.setAttribute('disabled', '');
+  });
+};
+
+const ALERT_SHOW_TIME = 5000;
+const error = document.querySelector('body');
+
+const showAlert = (message) => {
+  const alertError = document.createElement('div');
+  alertError.style.zIndex = 100;
+  alertError.style.position = 'absolute';
+  alertError.style.left = 0;
+  alertError.style.top = 0;
+  alertError.style.right = 0;
+  alertError.style.padding = '10px 3px';
+  alertError.style.fontSize = '30px';
+  alertError.style.textAlign = 'center';
+  alertError.style.backgroundColor = 'black';
+  alertError.style.color = 'red';
+
+  alertError.textContent = message;
+
+  error.append(alertError);
+  setTimeout(() => {
+    alertError.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+export { debounce, showAlert, disableFormFilters };
